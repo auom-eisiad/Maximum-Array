2ef3d93da50a37c74b87e58e605ae1894a018310
auom.eisiad <daisiemouaa@gmail.com>
2023-08-10 13:10:42 -0400

    add code to script

---
+++ script.js
@@ -0,0 +1,12 @@
+// function holding the maxArr
+function maxArr() {
+
+    // finds the max value from the array
+    var arr = Math.max(120, 123, 1, 1200);
+
+    // log the max value
+    console.log(arr);
+}
+
+// function calls itself
+maxArr();
